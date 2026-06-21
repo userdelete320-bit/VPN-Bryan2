05060dfe3ca0273da68eacdcc5f5ab8ec64a6520
userdelete320-bit <userdelete320@gmail.com>
2026-06-21 08:54:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -330,7 +330,7 @@ function buildMainMenuKeyboard(userId, firstName, esAdmin, isGroup = false) {
     const adminUrl = `${webappUrl}/admin.html?userId=${userId}&admin=true`;
     const inlineKeyboard = [
         [
-            createButton("VER PLANES", isGroup ? { url: plansUrl, style: 'success' } : { web_app: { url: plansUrl }, style: 'success' }),
+            createButton("VER PLANES", isGroup ? { url: plansUrl, style: 'primary' } : { web_app: { url: plansUrl }, style: 'success' }),
             createButton("MI PERFIL", { callback_data: "check_status" })
         ],
         [
